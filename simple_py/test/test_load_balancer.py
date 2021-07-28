4603f86d570f327584e5b8d45e3c5176acae2619
IlyaMoskva <i-moskva@ya.ru>
2021-07-28 12:41:52 +0300

    setup.py added

--- simple_py/test/test_load_balancer.py
+++ simple_py/test/test_load_balancer.py
@@ -3,14 +3,12 @@ import unittest
 from app import load_balancer
 
 
-class LoadBalancerAdd(unittest.TestCase):
+class TestLoadBalancer(unittest.TestCase):
 
-    def test_add_url(self):
+    def test_register_url(self):
         lb = load_balancer.LoadBalancer(2)
         self.assertTrue(lb.register_url("url1"))
         self.assertTrue(lb.register_url("url2"))
         self.assertFalse(lb.register_url("url3"))
+        print("Test OK")
 
-
-if __name__ == '__main__':
-    unittest.main()
